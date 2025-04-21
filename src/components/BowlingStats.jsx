44d25acbda0e0837b93bfcabfcb7084f443bd744
jwong65 <jason.wong6507@gmail.com>
2025-04-21 14:03:53 -0400

    Initalized BowlingStats component

--- src/components/BowlingStats.jsx
+++ src/components/BowlingStats.jsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { useThemeContext } from '../context/ThemeContext'
 
 export default function BowlingStats() {
+    const {themeMode} =useThemeContext()
+    const [loading, setLoading] = useState(true);
+    const [playerStats, setPlayerStats] = useState({})
+
+    useEffect(()=>{
+
+    },[])
+    
   return (
     <div>BowlingStats</div>
   )
